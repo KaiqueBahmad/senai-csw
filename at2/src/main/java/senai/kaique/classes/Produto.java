4b308a9c95d45fb2a7b55de7d5e8823db953f051
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-02-10 20:19:52 -0300

    atividade 2 feita

---
+++ at2/src/main/java/senai/kaique/classes/Produto.java
@@ -0,0 +1,45 @@
+package senai.kaique.classes;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidadeEstoque;
+
+
+    public void adicionarEstoque(int qtd) {
+        if (qtd <= 0) {
+            return;
+        }
+        quantidadeEstoque += qtd;
+    }
+
+    public void removerEstoque(ind qtd) {
+        if (quantidadeEstoque - qtd < 0) {
+            System.out.println("Você não pode remover mais do que há no estoque.");
+            return;
+        }
+        quantidadeEstoque -= qtd;
+    }
+
+
+    public void exibirInformacoes() {
+        System.out.printf("Nome: %s\nPreco: %.2f\nQuantidade Estoque: %d\n",nome, preco, quantidadeEstoque);
+    }
+
+    public String getNome() {
+        return nome;
+    } 
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+}
