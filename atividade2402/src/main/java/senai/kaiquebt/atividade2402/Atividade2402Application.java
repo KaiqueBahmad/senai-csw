8a20f28f54b1afe1313d12165d75b7b8dff81abb
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-02-24 19:49:01 -0300

    inicio da atividade 24 02

---
+++ atividade2402/src/main/java/senai/kaiquebt/atividade2402/Atividade2402Application.java
@@ -0,0 +1,13 @@
+package senai.kaiquebt.atividade2402;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade2402Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade2402Application.class, args);
+	}
+
+}
---
+++ atividade2402/src/test/java/senai/kaiquebt/atividade2402/Atividade2402ApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.kaiquebt.atividade2402;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade2402ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
