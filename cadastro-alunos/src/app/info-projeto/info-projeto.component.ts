2d9da7f22d8e9984543950fd509049542a3da648
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-04-07 14:59:28 -0300

    criado projeto

---
+++ cadastro-alunos/src/app/info-projeto/info-projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-projeto',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-projeto.component.html',
+  styleUrl: './info-projeto.component.css'
+})
+export class InfoProjetoComponent {
+
+}
