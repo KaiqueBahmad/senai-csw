e06e6f2b78fc48ef760eb60458704523c64fc3e9
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-05-19 19:48:51 -0300

    aluguel veiculo inicio

---
+++ aluguel-veiculo/back/src/test/java/senai/kaiquebt/aluguel_veiculo/AluguelVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.kaiquebt.aluguel_veiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
